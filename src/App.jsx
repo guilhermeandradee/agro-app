73aaf689f576a483df156a9add0ce1718fd02053
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-11-10 10:41:50 -0300

    add link

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import './App.css'
 import Header from './components/Header';
 import Posts from './components/Posts';
 import Footer from './components/Footer';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
 
-
+  const navigate = useNavigate()
 
   return (
     <>
         <main className='w-100 h-100-vh d-flex flex-column'>
             <Header />
-
+            <div className='w-100 pt-5 d-flex justify-content-center align-items-center'>
+              <a target='_blank' href='https://www.youtube.com/watch?v=jFXf_djogo4&feature=youtu.be' className='text-center text-black  m-0'>Link: https://www.youtube.com/watch?v=jFXf_djogo4&feature=youtu.be</a>
+            </div>
             <Posts />
 
             <Footer />
